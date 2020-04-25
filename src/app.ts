e941cc3165c8202a1ccee685d734932387cea3c9
Tuhin Roy <hadron@ip-192-168-43-167.ap-south-1.compute.internal>
2020-04-25 23:16:51 +0530

    add: wakatime base endpoint

---
+++ src/app.ts
@@ -0,0 +1,21 @@
+//ACCESS TOKEN = sec_rZLKZcjebOPkQykTTenzIhOlal2YlTfhQx2ZzH1pTKiSutGHM7YWdLL5BA3K37LTItzGXUGyAGWANcUK
+
+//REFRESH TOKEN = ref_o8vW5qbuZhTTO9jjajDLEDBb4ZdvegoQiDmZcgvOzoATIj7MOBLGluVzTxENQ37gR17zedQkGmTkV5TX
+
+//uid = 465cfbe4-2b6b-413c-88cf-9890bf9f5fb9
+
+//Token type - bearer
+
+//43200 EXPIRES_IN
+
+//ACCESS_TOKEN - sec_V1WlBt6Aa2kJE68bD7Hh1ucaZbBqXSmUHWmDnZSMfN5vrHmeDh59Gtu5vJhNjP0lXuG1ZmfXtDgXFNMm
+
+
+// {
+//     "access_token": "sec_V1WlBt6Aa2kJE68bD7Hh1ucaZbBqXSmUHWmDnZSMfN5vrHmeDh59Gtu5vJhNjP0lXuG1ZmfXtDgXFNMm",
+//     "expires_in": 5184000,
+//     "refresh_token": "ref_SCmsdL8RAiMJQVyKs4xr0aK9AOfRlE4FFMT0H9Yn2Z3jrPLf562s6AldxSYesIeZ9yYkXLTYliVZdIAu",
+//     "scope": "read_logged_time,read_stats,write_logged_time,email",
+//     "token_type": "bearer",
+//     "uid": "465cfbe4-2b6b-413c-88cf-9890bf9f5fb9"
+// }
